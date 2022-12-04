49c572f24cd2cf2abc442be9022dfbf7bde57168
Adrian Scripca <benishor@gmail.com>
2022-12-04 09:12:02 +0200

    Day 3: a bit of refactoring

--- day3/main.cpp
+++ day3/main.cpp
@@ -4,45 +4,40 @@ class day3 : public aoc::solution {
 protected:
 
     void run(std::istream& in, std::ostream& out) override {
+        uint64_t part1_sum{0}, part2_sum{0};
+        uint64_t group_badges[3] = {0};
+        std::string line;
 
-        uint32_t part1_sum{0}, part2_sum{0};
+        for (int line_index = 0; std::getline(in, line); line_index++) {
+            uint64_t badge_half_1{0};
+            uint64_t badge_half_2{0};
 
-        std::map<char, bool> seen_items;
-        int line_index = 0;
-        uint64_t badge_masks[3] = {0};
+            for (size_t i = 0; i < line.size(); i++) {
+                auto item_priority = priority_for_item(line[i]);
+                auto bit_to_set = 2UL << (item_priority - 1);
 
-        for (std::string line; std::getline(in, line); line_index++) {
-            seen_items.clear();
-            for (size_t i = 0; i < line.size() / 2; i++) {
-                seen_items[line[i]] = true;
-            }
-            for (size_t i = line.size() - 1; i >= line.size() / 2; i--) {
-                auto c = line[i];
-                if (seen_items.contains(c) && seen_items[c]) {
-                    part1_sum += priority_for_item(c);
-                    seen_items[c] = false;
+                if (i >= line.size() / 2) {
+                    badge_half_2 |= bit_to_set;
+                } else {
+                    badge_half_1 |= bit_to_set;
                 }
             }
+            part1_sum += log2(badge_half_1 & badge_half_2);
 
             auto badge_group_index = line_index % 3;
-            for (auto& c: line) {
-                badge_masks[badge_group_index] |= 2UL << priority_for_item(c);
-            }
-
+            group_badges[badge_group_index] = badge_half_1 | badge_half_2;
             if (badge_group_index == 2) {
-                auto badge = badge_masks[0] & badge_masks[1] & badge_masks[2];
-                part2_sum += log2(badge >> 1);
-                badge_masks[0] = badge_masks[1] = badge_masks[2] = 0;
+                part2_sum += log2(group_badges[0] & group_badges[1] & group_badges[2]);
+                group_badges[0] = group_badges[1] = group_badges[2] = 0;
             }
         }
         out << part1_sum << std::endl;
         out << part2_sum << std::endl;
     }
 
-    static uint32_t priority_for_item(char item) {
+    static uint64_t priority_for_item(char item) {
         return item >= 'A' && item <= 'Z' ? item - 'A' + 27 : item - 'a' + 1;
     }
-
 };
 
 int main(int argc, char **argv) {
